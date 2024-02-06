a9b575be662aee96bc2b8563d6497b49cefb158f
CoreSNK <125166144+CoreSNK@users.noreply.github.com>
2024-02-05 20:31:41 -0600

    took away sidebar to add navbar

--- app.py
+++ app.py
@@ -11,41 +11,6 @@ df2 = pd.read_csv("./Cleaned_Datasets/1spring_2020.csv")
 df3 = pd.concat([df, df2])
 total = df3["Cost Savings ($)"].sum()
 
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "10%",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
-
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-}
-
-sidebar = html.Div(
-    [
-        html.Hr(),
-        html.P("A simple sidebar layout with navigation links", className="lead"),
-        dbc.Nav(
-            [
-                dbc.NavLink("Home", href="/", active="exact"),
-                dbc.NavLink("Page 1", href="/page-1", active="exact"),
-                dbc.NavLink("Page 2", href="/page-2", active="exact"),
-            ],
-            vertical=True,
-            pills=True,
-        ),
-    ],
-    style=SIDEBAR_STYLE,
-)
-
-content = html.Div(id="page-content", style=CONTENT_STYLE)
-
 fig1 = px.histogram(
     df3,
     x="Department",
@@ -57,7 +22,6 @@ fig1.update_xaxes(categoryorder="total descending")
 
 app.layout = dbc.Container(
     [
-        html.Div([dcc.Location(id="url"), sidebar, content]),
         dbc.Row(
             [
                 html.Div(
