7492eb0ac48d3db68255d26b789895c7066ba9bb
CoreSNK <125166144+CoreSNK@users.noreply.github.com>
2024-02-06 12:49:26 -0600

    Centered up the navbar and gave it some gap

--- index.py
+++ index.py
@@ -3,31 +3,34 @@ import dash_bootstrap_components as dbc
 from app import app
 from pages import intro, year19_20
 
-app.layout = html.Div(
-    [
-        dcc.Location(id="url", refresh=False),
-        html.Div(
-            [
-                dcc.Link("Home", href="/"),
-                html.Br(),
-                dcc.Link("2019_20", href="/year19_20"),
-                html.Br(),
-                dcc.Link("2020-21", href="/"),
-                html.Br(),
-                dcc.Link("2021-22", href="/"),
-                html.Br(),
-                dcc.Link("2022-23", href="/"),
-                html.Br(),
-                dcc.Link("2023-24", href="/"),
-            ],
-            style={"padding": 10, "flex": 1},
-        ),
-        html.Div(id="page-content", style={"padding": 10}),
-    ]
+app.layout = dbc.Nav(
+    html.Div(
+        [
+            dcc.Location(id="url", refresh=False),
+            html.Div(
+                [
+                    dcc.Link("Home", href="/"),
+                    html.Br(),
+                    dcc.Link("2019_20", href="/year19_20"),
+                    html.Br(),
+                    dcc.Link("2020-21", href="/"),
+                    html.Br(),
+                    dcc.Link("2021-22", href="/"),
+                    html.Br(),
+                    dcc.Link("2022-23", href="/"),
+                    html.Br(),
+                    dcc.Link("2023-24", href="/"),
+                ],
+                style={"padding": 10, "flex": 1},
+                className="navbar d-flex justify-content-center gap-2 fs-4",
+            ),
+            html.Div(id="page-content", style={"padding": 10}),
+        ]
+    )
 )
 
 
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+@callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
     if pathname == "/":
         return intro.layout
