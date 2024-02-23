6f5f77918c3acc07d93023396954c976119c9753
CoreSNK <125166144+CoreSNK@users.noreply.github.com>
2024-02-23 15:59:40 -0600

    Made the top two graphs in a row

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import dash_bootstrap_components as dbc
 
 
 app = Dash(
-    __name__, external_stylesheets=[dbc.themes.LUX], suppress_callback_exceptions=True
+    __name__,
+    external_stylesheets=[dbc.themes.LUX],
+    suppress_callback_exceptions=True,
+    meta_tags=[
+        {
+            "name": "viewport",
+            "content": "width=device-width, initial-scale=1.0",
+        }
+    ],
 )
 server = app.server
