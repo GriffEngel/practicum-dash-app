2b36595b566cf2e918289ed42bbd696ca039b3d1
CoreSNK <gnengel07@gmail.com>
2024-03-11 15:28:06 -0500

    more unused code

--- pages/intro.py
+++ pages/intro.py
@@ -3,19 +3,6 @@ import pandas as pd
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
-
-# *========== Keep for Later if Needed ============
-# df = pd.read_csv("./Cleaned_Datasets/1fall_2019.csv")
-# df2 = pd.read_csv("./Cleaned_Datasets/1spring_2020.csv")
-# df3 = pd.read_csv("./Cleaned_Datasets/2summer_2020.csv")
-# df4 = pd.read_csv("./Cleaned_Datasets/3fall_2020.csv")
-# df5 = pd.read_csv("./Cleaned_Datasets/3spring_2021.csv")
-# df6 = pd.read_csv("./Cleaned_Datasets/4fall_2021.csv")
-# df7 = pd.read_csv("./Cleaned_Datasets/4spring_2022.csv")
-# df8 = pd.read_csv("./Cleaned_Datasets/5fall_2022.csv")
-# df9 = pd.read_csv("./Cleaned_Datasets/5spring_2023.csv")
-# df10 = pd.concat([df,df2,df3,df4,df5,df6,df7,df8,df9])
-
 # ---------------------------------------------------------------------------- #
 #                                    Graphs                                    #
 # ---------------------------------------------------------------------------- #
